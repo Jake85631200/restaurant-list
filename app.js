2a4874c778a494680355882b20934605081d915d
Jake85631200 <Jake85631200@gmail.com>
2024-05-02 11:48:40 +0800

    building route & controller setup

--- app.js
+++ app.js
@@ -9,6 +9,17 @@ app.get('/', (req, res) => {
   res.send('this a restaurant-listing website!')
 })
 
+// 將根路徑重新導向至restaurant.json
+app.get('/', (req, res) => {
+  res.redirect('/restaurant')
+})
+
+// 使用 params 做動態路由
+app.get('/restaurant/:id', (req, res) => {
+  const id = req.params.id
+  res.send(`reading restaurant id: ${id}`)
+})
+
 // 用來啟動伺服器
 app.listen(port, () => {
   console.log(`express server is running on http://localhost:${port}`)
