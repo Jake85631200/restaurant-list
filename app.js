4126e31f3c1dcf09af632d42de09cbabd92ab179
Jake85631200 <Jake85631200@gmail.com>
2024-05-04 23:06:28 +0800

    build: use express-handlebars as view engine

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
 // 載入 Express
 const express = require('express')
+const { engine } = require('express-handlebars')
+
 const app = express()
 
 // 設定路由
 const port = 3000
 
+app.engine('.hbs', engine({ extname: '.hbs' })) // extname: handlebars 的功能，用來指定樣版引使用的擴充文件名
+app.set('view engine', '.hbs')
+app.set('views', './views')
+
 // 將請求的 URL 與指定的路徑結合，以定位並返回相應的靜態資源文件。
 app.use(express.static('public'))
-
-app.get('/', (req, res) => {
-  res.send('this a restaurant-listing website!')
-})
 
 // 將根路徑重新導向至restaurant.json
 app.get('/', (req, res) => {
   res.redirect('/restaurant')
+})
+
+app.get('/restaurant', (req, res) => {
+  res.render('index')
 })
 
 // 使用 params 做動態路由
